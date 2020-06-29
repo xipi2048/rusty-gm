16dd23b422c813efc10bef69871655eb663efdf4
Raena Xi <raen@quantumtrip.org>
2020-06-29 01:07:10 -0700

    Add gdb build

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,13 @@ use cursive::views::{Dialog, TextView};
 fn main() {
     let mut siv = cursive::default();
 
-    siv.add_layer(Dialog::around(TextView::new("Hello...\nWould you like to play a game?"))
-                    .title("Raen's Dungeon")
-                    .button("Yes")
-                    .button("No", |s| s.quit()));
+    siv.add_layer(
+        Dialog::around(TextView::new("Hello...\nWould you like to play a game?"))
+            .title("Raen's Dungeon")
+            .button("Yes", |s| s.quit())
+            .button("No", |s| s.quit()),
+    );
+
     siv.run();
+    siv.clear();
 }
