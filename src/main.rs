3b50642b3cefa85a517799b109c495603b5d8754
xipi2048 <raen@quantumtrip.org>
2020-06-29 22:13:56 -0700

    Start on the game

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use cursive::views::{Dialog, TextView};
+use cursive::{
+    views::{Dialog, TextView},
+    Cursive,
+};
 
 fn main() {
     let mut siv = cursive::default();
@@ -6,10 +9,15 @@ fn main() {
     siv.add_layer(
         Dialog::around(TextView::new("Hello...\nWould you like to play a game?"))
             .title("Raen's Dungeon")
-            .button("Yes", |s| s.quit())
-            .button("No", |s| s.quit()),
+            .button("No", |s| s.quit())
+            .button("Yes", |s| start_game(s)),
     );
 
     siv.run();
     siv.clear();
+}
+
+fn start_game(siv: &mut Cursive) {
+    siv.pop_layer();
+    siv.add_layer(TextView::new("Thank you for playing ^_^"));
 }
