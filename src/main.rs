103a5d719eab0fabef1a38dc7f63defead83777c
xipi2048 <raen@quantumtrip.org>
2020-07-21 19:31:23 -0700

    Start on raylib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use raylib::prelude::*;
+
 fn main() {
-    
+    let w = 960;
+    let h = 540;
+
+    let (mut rl, thread) = raylib::init().size(w, h).title("Game Master").build();
+    let white = Color::new(255, 255, 255, 255);
+
+    rl.set_target_fps(60);
+
+    while !rl.window_should_close() {
+        let mut d = rl.begin_drawing(&thread);
+        d.clear_background(white);
+    }
 }
