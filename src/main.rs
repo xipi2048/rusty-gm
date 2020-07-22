14d59806cbec54d8196b5291bd1610034533e134
xipi2048 <raen@quantumtrip.org>
2020-07-21 21:41:34 -0700

    Refactor main

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,15 @@ fn main() {
     let w = 960;
     let h = 540;
 
-    let (mut rl, thread) = raylib::init().size(w, h).title("Game Master").build();
-    let white = Color::new(255, 255, 255, 255);
+    let (mut rl, thread) = raylib::init()
+        .size(w, h)
+        .title("Game Master")
+        .build();    
 
     rl.set_target_fps(60);
 
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
-        d.clear_background(white);
+        d.clear_background(Color::WHITE);
     }
 }
